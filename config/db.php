54d4f6c1d78dbe8e441a296b1bc7de81cdfafe12
Ivan Dzhuga <idjuga@gmail.com>
2016-07-31 16:04:42 +0300

    fix informations to readme

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=testObg',
+    'dsn' => 'mysql:host=localhost;dbname=testObg1',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
